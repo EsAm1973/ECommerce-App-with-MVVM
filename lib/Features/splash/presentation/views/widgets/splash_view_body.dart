3a3765fd41c75b7306fe56ea8d54462cc2dda434
EsAm1973 <me354172@gmail.com>
2025-02-06 11:53:04 +0200

    Make a design of splash view

---
+++ lib/Features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,53 @@
+import 'package:ecommerce_app/Core/utils/assets.dart';
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  double _opacirty = 0.0;
+  @override
+  void initState() {
+    super.initState();
+    _animatedOpacity();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        AnimatedImage(),
+      ],
+    );
+  }
+
+  AnimatedOpacity AnimatedImage() {
+    return AnimatedOpacity(
+      duration: const Duration(milliseconds: 500),
+      opacity: _opacirty,
+      curve: Curves.easeInOut,
+      child: Image.asset(
+        AssetsPath.logo,
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
+
+  void _animatedOpacity() {
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() {
+        _opacirty = 1.0;
+      });
+      // Future.delayed(const Duration(milliseconds: 500), () {
+      //   GoRouter.of(context).push(AppRouter.kHomeView);
+      // });
+    });
+  }
+}
