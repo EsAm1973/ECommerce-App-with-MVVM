8d67188205e4e60a21e4e14bd770c5c1be5c6bba
EsAm1973 <me354172@gmail.com>
2025-02-07 18:13:08 +0200

    create login repo implement

---
+++ lib/Features/Authenticate/data/repos/login_repo_implement.dart
@@ -0,0 +1,33 @@
+import 'package:dartz/dartz.dart';
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/Core/errors/failure.dart';
+import 'package:ecommerce_app/Core/utils/api_service.dart';
+import 'package:ecommerce_app/Features/Authenticate/data/repos/auth_repo.dart';
+
+class LoginRepoImplement implements LoginRepo {
+  final ApiService apiService;
+
+  LoginRepoImplement({required this.apiService});
+  @override
+  Future<Either<Failure, Map<String, dynamic>>> login(
+      {required String email, required String password}) async {
+    final body = {
+      'email': email,
+      'password': password,
+    };
+    final headers = {
+      'lang': 'en',
+      'Content-Type': 'application/json',
+    };
+    try {
+      final response = await apiService.post('login', body, headers: headers);
+      return right(response);
+    } on Exception catch (e) {
+      if (e is DioException) {
+        return left(ServerFailure.fromDioException(e));
+      } else {
+        return left(ServerFailure(errorMessage: e.toString()));
+      }
+    }
+  }
+}
