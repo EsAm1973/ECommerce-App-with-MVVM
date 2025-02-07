5208a902557c3999b743541ecf9a9bcecf1698d2
EsAm1973 <me354172@gmail.com>
2025-02-07 17:43:20 +0200

    create api service

--- lib/Core/utils/api_service.dart
+++ lib/Core/utils/api_service.dart
@@ -1,3 +1,26 @@
+import 'package:dio/dio.dart';
+import 'package:ecommerce_app/constants.dart';
+
 class ApiService {
-  
+  final Dio dio;
+
+  ApiService({Dio? dio})
+      : dio = dio ??
+            Dio(BaseOptions(
+              baseUrl: baseUrl,
+              headers: {'Content-Type': 'application/json'},
+            ));
+
+  Future<dynamic> get(String endPoint, {Map<String, dynamic>? headers}) async {
+    final response =
+        await dio.get(endPoint, options: Options(headers: headers));
+    return response.data;
+  }
+
+  Future<dynamic> post(String endPoint, Map<String, dynamic>? data,
+      {Map<String, dynamic>? headers}) async {
+    final response = await dio.post(endPoint,
+        data: data, options: Options(headers: headers));
+    return response.data;
+  }
 }
