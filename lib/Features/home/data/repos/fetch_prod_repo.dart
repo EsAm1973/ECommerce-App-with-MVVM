9eed90ef075d043e59908e89343df107011492dd
EsAm1973 <me354172@gmail.com>
2025-02-12 17:18:01 +0200

    create fetch product repo

---
+++ lib/Features/home/data/repos/fetch_prod_repo.dart
@@ -0,0 +1,7 @@
+import 'package:dartz/dartz.dart';
+import 'package:ecommerce_app/Core/errors/failure.dart';
+import 'package:ecommerce_app/Features/home/data/models/productModel.dart';
+
+abstract class FetchProductRepo {
+  Either<Failure, List<Product>> fetchProducts();
+}
